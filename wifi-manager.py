ad3a4ff7340a92ce39efa4553540e265da53bbfa
Benjamin Häublein <benjaminh@benjamin-VAIO.lan.benjaminh.de>
2015-04-04 14:50:10 +0200

    showing "Table"

--- wifi-manager.py
+++ wifi-manager.py
@@ -10,6 +10,11 @@ def parseArgs():
     parser = argparse.ArgumentParser(description='Manage list of wifi networks')
     parser.add_argument('--filter', action='store', help='search for WIFI network')
     parser.add_argument('--path', action='store', help='use file as database')
+    parser.add_argument('--connect', action='store_true', help='connect to first network in list')
+    parser.add_argument('--add', action='store_true', help='add network')
+    parser.add_argument('--remove', action='store_true', help='remove all found entries')
+    parser.add_argument('--replace', action='store_true', help='replace network')
+    parser.add_argument('--parse-add', action='store_true', help='add multiple networks by parsing standard input')
     args = parser.parse_args()
     return args
 
@@ -27,23 +32,20 @@ def main():
     #palette = [('entry', 'default,bold', 'default')]
     #urwid.MainLoop(Table((formatWifiGen((data)))), palette).run()
     # start display
-    minWidth = [10 for i in range(0,4)]
+    minWidth = [15 for i in range(0,4)]
     for wifi in data:
         print(formatWifi(wifi, minWidth))
         
 def fillWidth(strings, minWidth):
     if minWidth == None:
         return strings
+    newStrings = ['' for i in range(0, len(strings)-1)]
     for i in range(0,min(len(minWidth),len(strings))-1):
-        newStrings[i] = strings[i]+' '*(minWidth-len(strings[i]))
+        newStrings[i] = strings[i]+' ' * (minWidth[i]-len(strings[i]))
     return newStrings
 
 def formatWifi(wifi, minWidth = None):
     return ' | '.join(fillWidth([wifi.name,wifi.ssid,wifi.location,wifi.comment],minWidth))
-
-def formatWifiGen(data):
-    for wifi in data():
-        yield formatWifi(wifi)
 
 if __name__ =='__main__':
     main()
