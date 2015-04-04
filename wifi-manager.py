89d3bc9e85461860760860557d12570a607f8355
Benjamin Häublein <benjaminh@benjamin-VAIO.lan.benjaminh.de>
2015-04-04 15:04:51 +0200

    addWifi stub

--- wifi-manager.py
+++ wifi-manager.py
@@ -10,11 +10,11 @@ def parseArgs():
     parser = argparse.ArgumentParser(description='Manage list of wifi networks')
     parser.add_argument('--filter', action='store', help='search for WIFI network')
     parser.add_argument('--path', action='store', help='use file as database')
-    parser.add_argument('--connect', action='store_true', help='connect to first network in list')
+    parser.add_argument('--connect', action='store', help='connect to first network in list')
     parser.add_argument('--add', action='store_true', help='add network')
-    parser.add_argument('--remove', action='store_true', help='remove all found entries')
-    parser.add_argument('--replace', action='store_true', help='replace network')
-    parser.add_argument('--parse-add', action='store_true', help='add multiple networks by parsing standard input')
+    parser.add_argument('--remove', action='store', help='remove all found entries')
+    parser.add_argument('--replace', action='store', help='replace network')
+    parser.add_argument('--parse-add', action='store', help='add multiple networks by parsing standard input')
     args = parser.parse_args()
     return args
 
@@ -24,6 +24,8 @@ def main():
     if args.path != None:
         path = args.path
     db = database.DB(path)
+    if args.add == True:
+        addWifi(db) 
     if args.filter != None:
         data = db.filteredData(args.filter)
     else:
@@ -32,9 +34,12 @@ def main():
     #palette = [('entry', 'default,bold', 'default')]
     #urwid.MainLoop(Table((formatWifiGen((data)))), palette).run()
     # start display
-    minWidth = [15 for i in range(0,4)]
+    minWidth = [15 for i in range(0,5)]
+    minWidth[0] = 2
+    id = 0
     for wifi in data:
-        print(formatWifi(wifi, minWidth))
+        id += 1
+        print(formatWifi(id, wifi, minWidth))
         
 def fillWidth(strings, minWidth):
     if minWidth == None:
@@ -44,8 +49,11 @@ def fillWidth(strings, minWidth):
         newStrings[i] = strings[i]+' ' * (minWidth[i]-len(strings[i]))
     return newStrings
 
-def formatWifi(wifi, minWidth = None):
-    return ' | '.join(fillWidth([wifi.name,wifi.ssid,wifi.location,wifi.comment],minWidth))
+def formatWifi(id, wifi, minWidth = None):
+    return ' | '.join(fillWidth([str(id), wifi.name, wifi.ssid, wifi.location, wifi.comment],minWidth))
+
+def addWifi(db):
+    pass
 
 if __name__ =='__main__':
     main()
